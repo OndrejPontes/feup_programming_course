1f988d285628d2746898394555ba3ae3000994e4
Ondrej Ponteš <ondrej.ponte@abo.fi>
2020-03-03 13:28:32 +0000

    Initial commit

---
+++ lab_01.cpp
@@ -0,0 +1,205 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <regex>
+#include <string>
+#include <sstream>
+#include <vector>
+
+void task11()
+{
+    char x;
+
+    std::cout << "Task 1.1 Write any characterstd::endl";
+    std::cin >> x;
+    std::cout << "ASCII value of character " << x << "is: " << int(x) << std::endl;
+}
+
+void task12()
+{
+    int a, b, c;
+    float mean;
+
+    std::cout << "Task 1.2 Please, input 3 integer number" << std::endl;
+
+    std::cout << "A ? ";
+    std::cin >> a;
+    std::cout << "B ? ";
+    std::cin >> b;
+    std::cout << "C ? ";
+    std::cin >> c;
+
+    mean = (a + b + c) / (float)3;
+
+    std::cout << std::fixed;
+    std::cout << std::setprecision(2);
+
+    std::cout << "mean   = " << mean << std::endl;
+    std::cout << "A-mean = " << mean - a << std::endl;
+    std::cout << "B-mean = " << mean - b << std::endl;
+    std::cout << "C-mean = " << mean - c << std::endl;
+}
+
+void task13()
+{
+    float p, r;
+
+    std::cout << "Task 1.3 Sphere mass calculator" << std::endl;
+
+    std::cout << "Enter material in Kg/m^3: ";
+    std::cin >> p;
+    std::cout << "Enter radius in m: ";
+    std::cin >> r;
+
+    std::cout << "Mass of the sphere is: " << 4 / 3 * (p * M_PI * r) << "Kg" << std::endl;
+}
+
+void task14()
+{
+    int a, b, c, d, e, f;
+    float x, y;
+
+    std::cout << "Task 1.4 Linear equation solver for two variables" << std::endl
+        << "Enter proper values to substitute" << std::endl << "a * x + b * y = c" << std::endl << "d * x + e * y = f " << std::endl;
+
+    std::cout << "Enter a: ";
+    std::cin >> a;
+    std::cout << "Enter b: ";
+    std::cin >> b;
+    std::cout << "Enter c: ";
+    std::cin >> c;
+    std::cout << "Enter d: ";
+    std::cin >> d;
+    std::cout << "Enter e: ";
+    std::cin >> e;
+    std::cout << "Enter f: ";
+    std::cin >> f;
+
+    x = (float)(c * e - b * f) / (float)(a * e - b * d);
+    y = (float)(a * f - c * d) / (float)(a * e - b * d);
+
+    std::cout << "x = " << x;
+    std::cout << "y = " << y;
+
+}
+
+std::vector<int> getValuesFromTimeString(std::string text)
+{
+    const std::regex delimiters("[^\\s.,-:;!?]+");
+    std::vector<int> result;
+    int x;
+
+    auto words_begin = std::sregex_iterator(text.begin(), text.end(), delimiters);
+    auto words_end = std::sregex_iterator();
+
+    for (std::sregex_iterator i = words_begin; i != words_end; ++i)
+    {
+        std::istringstream((*i).str()) >> x;
+        result.push_back(x);
+    }
+    return result;
+}
+
+void task15()
+{
+    std::string time1, time2;
+    int h, m, s;
+    std::vector<int> time1_vec, time2_vec;
+
+    std::cout << "Task 1.5 Time counter" << std::endl;
+    std::cout << "Time1 (hours minutes seconds) ? ";
+    std::getline(std::cin, time1);
+    std::cout << "Time2 (hours minutes seconds) ? ";
+    std::getline(std::cin, time2);
+
+    time1_vec = getValuesFromTimeString(time1);
+    time2_vec = getValuesFromTimeString(time2);
+
+    s = time1_vec.at(2) + time2_vec.at(2);
+    m = time1_vec.at(1) + time2_vec.at(1) + s / 60;
+    h = time1_vec.at(0) + time2_vec.at(0) + m / 60;
+
+    std::cout << "Time1 + Time2 = " << h / 24 << " days, " << h % 24 << " hours, " << m % 60 << " minutes, " << s % 60 << " seconds." << std::endl;
+}
+
+class Vertex {
+private:
+    int x, y;
+
+public:
+    Vertex()
+    {
+        x = 0;
+        y = 0;
+    }
+
+    Vertex(int x1, int y1)
+    {
+        x = x1;
+        y = y1;
+    }
+
+    double distanceFrom(Vertex v)
+    {
+        return std::sqrt(std::pow(v.x - x, 2) + std::pow(v.y - y, 2));
+    }
+};
+
+class Triangle
+{
+private:
+    Vertex v1, v2, v3;
+public:
+    Triangle(Vertex c1, Vertex c2, Vertex c3)
+    {
+        v1 = c1;
+        v2 = c2;
+        v3 = c3;
+    }
+
+    double area(double s)
+    {
+        return std::sqrt(s * (s - v1.distanceFrom(v2)) * (s - v2.distanceFrom(v3)) * (s - v3.distanceFrom(v1)));
+    }
+};
+
+void task16()
+{
+    int x, y;
+    double semiperimeter;
+    Vertex a, b, c;
+
+    std::cout << "Task 1.6: Triangle area counter" << std::endl;
+
+    std::cout << "Enter x of vertex A: ";
+    std::cin >> x;
+    std::cout << "Enter y of vertex A: ";
+    std::cin >> y;
+    a = Vertex(x, y);
+
+    std::cout << "Enter x of vertex B: ";
+    std::cin >> x;
+    std::cout << "Enter y of vertex B: ";
+    std::cin >> y;
+    b = Vertex(x, y);
+
+    std::cout << "Enter x of vertex C: ";
+    std::cin >> x;
+    std::cout << "Enter y of vertex C: ";
+    std::cin >> y;
+    c = Vertex(x, y);
+
+    Triangle t = Triangle(a, b, c);
+
+    std::cout << "Enter semiperimeter: ";
+    std::cin >> semiperimeter;
+
+    std::cout << "Area of your triangle is: " << t.area(semiperimeter);
+}
+
+int main()
+{
+    task16();
+}
